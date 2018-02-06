62df51256ac2d2e5d1d86a66ec7471eff85b4694
Jos Castro <joselycas@gmail.com>
2018-02-06 16:53:11 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,7 +61,7 @@ class Game
   def play
     turn until over?
        if won?
-         puts "Congratulations #{winner(board)}!"
+         puts "Congratulations #{winner}!"
        elsif draw?
          puts "Cat's Game!"
        end
